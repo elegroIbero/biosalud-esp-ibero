b47c9223e4218438842aea5a0a001f5f85feb10d
edwin andres legro agudelo <maxedwwin@gmail.com>
2024-05-29 22:45:59 -0500

    feat: :art: actualizando y ajustando los estilos scss

--- src/includes/Menu.jsx
+++ src/includes/Menu.jsx
@@ -4,18 +4,19 @@ import Container from 'react-bootstrap/Container';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 import logo from '/logo-imagen.png';
+import 'bootstrap/scss/bootstrap-utilities.scss';
 
 const Menu = () => {
 	return (
-		<Navbar collapseOnSelect expand='lg' fixed='top' sticky='bottom'>
+		<Navbar collapseOnSelect expand='lg' fixed='top' sticky='bottom' as='navbar'>
 			<Container>
 				<Link className='navbar-brand' to='/'>
-					<img src={logo} className='img-responsive' alt='Vite logo' width={50} />
-					<span className='m-2'>BioSalud S.A.S</span>
+					<img src={logo} className='d-inline-block align-text-top' alt='Vite logo' width={40} />
+					<span className='h4'>  BioSalud S.A.S</span>
 				</Link>
-				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+				<Navbar.Toggle aria-controls='responsive-navbar-nav'/>
 				<Navbar.Collapse id='responsive-navbar-nav'>
-					<Nav className='me-auto my-2 my-lg-0'>
+					<Nav className='me-auto my-2 my-lg-0' as='nav'>
 						<Link className='nav-link' to='/'>
 							Información
 						</Link>
@@ -25,7 +26,7 @@ const Menu = () => {
 						<Link className='nav-link' to='/contact'>
 							Contacto
 						</Link>
-						<NavDropdown title='Cuenta' id='collapsible-nav-dropdown'>
+						<NavDropdown title='Cuenta' id='collapsible-nav-dropdown-01' className='d-lg-none d-xl-none'>
 							<Link className='dropdown-item' to='/'>
 								Iniciar sesión
 							</Link>
@@ -35,6 +36,17 @@ const Menu = () => {
 						</NavDropdown>
 					</Nav>
 				</Navbar.Collapse>
+
+				<Nav className='d-none d-lg-block' as='nav'>
+					<NavDropdown title='Cuenta' id='collapsible-nav-dropdown-02'>
+						<Link className='dropdown-item' to='/'>
+							Iniciar sesión
+						</Link>
+						<Link className='dropdown-item' to='/home'>
+							Registrar
+						</Link>
+					</NavDropdown>
+				</Nav>
 			</Container>
 		</Navbar>
 	);
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { App } from './App';
 import { Home } from './components/Home';
 import { Contact } from './components/Contact';
 import { Error404 } from './components/Error404';
 import { Portafolio } from './components/Portafolio';
+import { useEffect } from 'react';
+
+const ScrollToTop = function () {
+	const { pathname } = useLocation();
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+	return null;
+};
 
 const AppRoutes = function () {
 	return (
 		<App>
+			<ScrollToTop />
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/home' element={<Home />} />
